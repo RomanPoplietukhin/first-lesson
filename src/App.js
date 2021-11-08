4fcf2a15937d4f5b68e5b381096045c2d09b89c9
Roman <romapopletuhin@gmail.com>
2021-11-08 16:05:56 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const slides = document.querySelectorAll('.slider-item');
+  let startPosition = 0;
+
+  function slideNextImage(e) {
+    slides[startPosition].style.display = 'none'; 
+    slides[++startPosition].style.display = 'block';
+    if (startPosition === slides.length - 1) { 
+      e.target.style.display = 'none';
+    }
+  };
+
+  function slidePrevImage(e) {
+    slides[startPosition].style.display = 'none';
+    slides[--startPosition].style.display = 'block';
+    if (startPosition === 0) {
+      e.target.style.display = 'none';
+    }
+  }
+
+  function createText() {
+    return {__html: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo vitae, modi rerum beatae provident
+    explicabo exercitationem doloremque sunt. Labore minima ratione praesentium nihil magnam necessitatibus 
+    illum commodi laboriosam et cum.`}
+  };
+
+  const props = {
+    className: "welcome-text",
+    id: "content",
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container"> 
+      <h1 className="title"> { 
+        'welcome to React'.split(' ').map((el, index) => index % 2 === 0 ? el.toUpperCase() + ' ' : el + ' ')
+      } </h1>
+      <p dangerouslySetInnerHTML={ createText() } { ...props } ></p>
+      <div className="slider-container">
+        <div className="slider-item"><img className="slider-item__img" src= {  logo } alt="logo"/>1</div>
+        <div className="slider-item"><img className="slider-item__img" src= {  logo } alt="logo"/>2</div>
+        <div className="slider-item"><img className="slider-item__img" src= {  logo } alt="logo"/>3</div>
+        <button className="slider-btn prev" onClick={ slideNextImage }>
+          <img src={ logo } className="prev" alt="logo"/>
+        </button>
+        <button className="slider-btn next" onClick={ slidePrevImage }>
+          <img src={ logo } className="next" alt="logo"/>
+        </button>
+      </div>
     </div>
   );
 }
